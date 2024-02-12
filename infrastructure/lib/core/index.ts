d79b63d7c08beebe6380a329c0b5205263a55736
isomer04 <43922158+isomer04@users.noreply.github.com>
2024-02-12 18:46:19 -0500

    Added deployment configuration for web application

--- infrastructure/lib/core/index.ts
+++ infrastructure/lib/core/index.ts
@@ -2,7 +2,6 @@ import * as cdk from '@aws-cdk/core';
 import { AssetStorage } from './storage';
 import { WebApp } from './webapp';
 
-
 export class ApplicationStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -14,7 +13,9 @@ export class ApplicationStack extends cdk.Stack {
     const storage = new AssetStorage(this, 'Storage');
 
     new WebApp(this, 'WebApp', {
-      hostingBucket: storage.hostingBucket
+      hostingBucket: storage.hostingBucket,
+      baseDirectory: '../',
+      relativeWebAppPath: 'webapp'
     });
   }
 }
